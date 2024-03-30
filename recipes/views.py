e34ba96949e66f3d4bab5259626bce487d0c17b8
Paulo <pdichone@gmail.com>
2024-03-30 14:45:47 -0700

    greater than field lookup

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,23 @@
+from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Recipe
+from django.db.models import Avg, Count, Sum
 
 
 # Create your views here.
 def recipes(request):
     # recipes = Recipe.objects.filter(category__name__exact="Soup")
     # recipes = Recipe.objects.exclude(name__contains="chocolate")
-    recipes = (
-        Recipe.objects.filter(category__name__exact="Soup")
-        .exclude(name__contains="chocolate")
-        .order_by("-date_added")
-    )
+    # recipes = (
+    #     Recipe.objects.filter(category__name__exact="Soup")
+    #     .exclude(name__contains="chocolate")
+    #     .order_by("-date_added")
+    # )
+    # recipes = Recipe.objects.all()[:2]
+    # recipes = Recipe.objects.aggregate(Count("id"))
+    # recipes = Recipe.objects.aggregate(Avg("id"))
+    recipes = Recipe.objects.filter(id__gt=3)
 
     print("Recipes", recipes)
 

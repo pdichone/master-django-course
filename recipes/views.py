d7c2e0a14da7d58d004c061c7c5f80d5ce62d94f
Paulo <pdichone@gmail.com>
2024-03-30 14:21:00 -0700

    chaining filters and ordering by

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,12 @@ from .models import Recipe
 # Create your views here.
 def recipes(request):
     # recipes = Recipe.objects.filter(category__name__exact="Soup")
-    recipes = Recipe.objects.exclude(name__contains="chocolate")
+    # recipes = Recipe.objects.exclude(name__contains="chocolate")
+    recipes = (
+        Recipe.objects.filter(category__name__exact="Soup")
+        .exclude(name__contains="chocolate")
+        .order_by("-date_added")
+    )
 
     print("Recipes", recipes)
 

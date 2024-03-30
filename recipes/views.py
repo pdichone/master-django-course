80796be5caf71a087bf3e0ffe84326cb8a8958dd
Paulo <pdichone@gmail.com>
2024-03-30 13:33:53 -0700

    filter lookups

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Recipe
 
 
 # Create your views here.
 def recipes(request):
+    recipes = Recipe.objects.filter(category__name__exact="Soup")
+    print("Recipes", recipes)
+
     return HttpResponse("Hello from Recipes")

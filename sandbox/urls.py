765f9c6690ed2af69cd2a634ffadcd3542bdfa58
Paulo <pdichone@gmail.com>
2024-04-08 13:47:16 -0700

    add session and feedback review step

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("recipes/<int:pk>", views.RecipeDetailView.as_view(), name="recipe_detail"),
     path("refreshing/", views.SpecifcRecipesView.as_view(), name="refreshing_recipes"),
     path("feedback/", views.feedback, name="feedback"),
+    path("feedback/review", views.feedback_review, name="feedback_review"),
     path("thank-you/", views.thank_you, name="thank_you"),
 ]
--- sandbox/views.py
+++ sandbox/views.py
@@ -12,6 +12,7 @@ from django.contrib import messages
 
 # Create your views here.
 def index(request):
+
     recipes = Recipe.objects.all()
     context = {"recipes": recipes}
     return render(request, "sandbox/index.html", context)
@@ -39,30 +40,46 @@ def thank_you(request):
 
 
 def feedback(request):
+    request.session["feedback_visits"] = request.session.get("feedback_visits", 0) + 1
+
     if request.method == "POST":
         form = FeedbackForm(request.POST)
         if form.is_valid():
-            # process the form
-            # print(form.cleaned_data)
-            name = form.cleaned_data["name"]
-            email = form.cleaned_data["email"]
-            feedback = form.cleaned_data["feedback"]
-            satisfaction = form.cleaned_data["satisfaction"]
-            Feedback.objects.create(
-                name=name, email=email, feedback=feedback, satisfaction=satisfaction
-            )
+
+            # save form data to session
+            request.session["feedback_data"] = form.cleaned_data
+            return redirect("sandbox:feedback_review")
+
+            # name = form.cleaned_data["name"]
+            # email = form.cleaned_data["email"]
+            # feedback = form.cleaned_data["feedback"]
+            # satisfaction = form.cleaned_data["satisfaction"]
+            # Feedback.objects.create(
+            #     name=name, email=email, feedback=feedback, satisfaction=satisfaction
+            # )
             # messages.add_message(
             #     request, messages.SUCCESS, "Feedback sent successfully!"
             # )
-            messages.success(request, "Feedback sent successfully!")
+            # messages.success(request, "Feedback sent successfully!")
 
-            return redirect("sandbox:index")
+            # return redirect("sandbox:index")
 
     else:
         form = FeedbackForm()
 
-    context = {"form": form}
+    context = {"form": form, "visits": request.session["feedback_visits"]}
     return render(request, "sandbox/feedback_form.html", context)
+
+
+def feedback_review(request):
+    feedback_data = request.session.get("feedback_data", {})
+    if request.method == "POST":
+        Feedback.objects.create(**feedback_data)
+        del request.session["feedback_data"]
+        return redirect("sandbox:index")
+
+    form = FeedbackForm(initial=feedback_data)
+    return render(request, "sandbox/feedback_review.html", {"form": form})
 
 
 class SpecifcRecipesView(View):
